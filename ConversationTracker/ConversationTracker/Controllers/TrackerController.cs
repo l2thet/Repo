e56b490ad93a82f4b17be8b4c81ec0c23c0e2375
l2thet <daltvet@hotmail.com>
2015-05-07 17:55:20 -0400

    Changing the Add Method to use MongoDB
    Changing the Add Method to use MongoDB

--- ConversationTracker/ConversationTracker/Controllers/TrackerController.cs
+++ ConversationTracker/ConversationTracker/Controllers/TrackerController.cs
@@ -30,19 +30,8 @@ namespace ConversationTracker.Controllers
             var url = new MongoUrl(connectionstring);
             var client = new MongoClient(url);
             var db = client.GetDatabase(url.DatabaseName);
-
             var collection = db.GetCollection<ConversationTrackerObject>("conversations");
-            var conversation = new ConversationTrackerObject { 
-                Date = DateTime.Now,
-                NotesOfChangeOverTime = "none",
-                RateOfUnease = 0,
-                SettingOrEnvironment = "home",
-                Who = "me?!"
-            };
-
-            collection.InsertOneAsync(conversation).Wait();
-
-            Task<List<ConversationTrackerObject>> waiting = collection.Find(x => x.Who == "me?!").ToListAsync();
+            Task<List<ConversationTrackerObject>> waiting = collection.Find(x => x.RateOfUnease >= 0).ToListAsync();
             waiting.Wait();
             fromDb = waiting.Result;
 
@@ -52,18 +41,14 @@ namespace ConversationTracker.Controllers
         [HttpPost]
         public JsonResult saveTrackedConversation(ConversationTrackerObject convo)
         {
-            //using (IDocumentStore store = new DocumentStore
-            //{
-            //    Url = "mongodb://appharbor_62031bdd-72a5-4bf1-9b12-4977e6e6b49b:e91nm4msb5f4k87kblf7ha42pi@ds031972.mongolab.com:31972/appharbor_62031bdd-72a5-4bf1-9b12-4977e6e6b49b"
-            //})
-            //{
-            //    store.Initialize();
-            //    using (IDocumentSession session = store.OpenSession())
-            //    {
-            //        session.Store(convo);
-            //        session.SaveChanges();
-            //    }
-            //}
+            var connectionstring = ConfigurationManager.AppSettings.Get("MONGOLAB_URI");
+            var url = new MongoUrl(connectionstring);
+            var client = new MongoClient(url);
+            var db = client.GetDatabase(url.DatabaseName);
+            var collection = db.GetCollection<ConversationTrackerObject>("conversations");
+
+            collection.InsertOneAsync(convo).Wait();
+            
             return Json(convo);
         }
 

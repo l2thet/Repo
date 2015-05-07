fe4020b8f73f0fa9d5d384864a07780bc4f05d99
l2thet <daltvet@hotmail.com>
2015-05-06 23:24:26 -0400

    Adding the basic solution into GitHub
    All the files!

---
+++ ConversationTracker/ConversationTracker/Controllers/TrackerController.cs
@@ -0,0 +1,80 @@
+﻿using Raven.Client;
+using Raven.Client.Document;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ConversationTracker.Models;
+using Raven.Abstractions.Commands;
+
+namespace ConversationTracker.Controllers
+{
+    public class TrackerController : Controller
+    {
+        //
+        // GET: /Tracker/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult retrieveTrackedConversations()
+        {
+            List<ConversationTrackerObject> fromDb;
+
+            using (IDocumentStore store = new DocumentStore
+            {
+                Url = "http://localhost:8080/"
+            })
+            {
+                store.Initialize();
+                using (IDocumentSession session = store.OpenSession())
+                {
+                    fromDb = session.Query<ConversationTrackerObject>().ToList();
+                }
+            }
+
+            return Json(fromDb, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult saveTrackedConversation(ConversationTrackerObject convo)
+        {
+            using (IDocumentStore store = new DocumentStore
+            {
+                Url = "http://localhost:8080"
+            })
+            {
+                store.Initialize();
+                using (IDocumentSession session = store.OpenSession())
+                {
+                    session.Store(convo);
+                    session.SaveChanges();
+                }
+            }
+            return Json(convo);
+        }
+
+        [HttpPost]
+        public JsonResult deleteTrackedConversation(ConversationTrackerObject convo)
+        {
+            List<ConversationTrackerObject> fromDb;
+            using (IDocumentStore store = new DocumentStore
+            {
+                Url = "http://localhost:8080"
+            })
+            {
+                store.Initialize();
+                using (IDocumentSession session = store.OpenSession())
+                {
+                    session.Advanced.Defer(new DeleteCommandData { Key = convo.Id });
+                    session.SaveChanges();
+                    fromDb = session.Query<ConversationTrackerObject>().ToList();
+                }
+            }
+            return Json(fromDb);
+        }
+    }
+}
---
+++ ConversationTracker/ConversationTracker/Models/ConversationTrackerObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConversationTracker.Models
+{
+    public class ConversationTrackerObject
+    {
+        public string Id { get; set; }
+        public DateTime Date { get; set; }
+        public string SettingOrEnvironment { get; set; }
+        public string Who { get; set; }
+        public int RateOfUnease { get; set; }
+        public string NotesOfChangeOverTime { get; set; }
+    }
+}

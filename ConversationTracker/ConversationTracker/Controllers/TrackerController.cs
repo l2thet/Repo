8e7d70098e20276812852ed879df512612bc5e54
l2thet <daltvet@hotmail.com>
2015-05-07 17:29:58 -0400

    Changing the connection from RavenDB to MongoDB
    Mongo mad!!!!!!!!!!

--- ConversationTracker/ConversationTracker/Controllers/TrackerController.cs
+++ ConversationTracker/ConversationTracker/Controllers/TrackerController.cs
@@ -1,12 +1,14 @@
-﻿using Raven.Client;
-using Raven.Client.Document;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ConversationTracker.Models;
-using Raven.Abstractions.Commands;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Configuration;
+using System.Threading.Tasks;
+
 
 namespace ConversationTracker.Controllers
 {
@@ -24,17 +26,25 @@ namespace ConversationTracker.Controllers
         {
             List<ConversationTrackerObject> fromDb;
 
-            using (IDocumentStore store = new DocumentStore
-            {
-                Url = "mongodb://appharbor_62031bdd-72a5-4bf1-9b12-4977e6e6b49b:e91nm4msb5f4k87kblf7ha42pi@ds031972.mongolab.com:31972/appharbor_62031bdd-72a5-4bf1-9b12-4977e6e6b49b"
-            })
-            {
-                store.Initialize();
-                using (IDocumentSession session = store.OpenSession())
-                {
-                    fromDb = session.Query<ConversationTrackerObject>().ToList();
-                }
-            }
+            var connectionstring = ConfigurationManager.AppSettings.Get("MONGOLAB_URI");
+            var url = new MongoUrl(connectionstring);
+            var client = new MongoClient(url);
+            var db = client.GetDatabase(url.DatabaseName);
+
+            var collection = db.GetCollection<ConversationTrackerObject>("conversations");
+            var conversation = new ConversationTrackerObject { 
+                Date = DateTime.Now,
+                NotesOfChangeOverTime = "none",
+                RateOfUnease = 0,
+                SettingOrEnvironment = "home",
+                Who = "me?!"
+            };
+
+            collection.InsertOneAsync(conversation).Wait();
+
+            Task<List<ConversationTrackerObject>> waiting = collection.Find(x => x.Who == "me?!").ToListAsync();
+            waiting.Wait();
+            fromDb = waiting.Result;
 
             return Json(fromDb, JsonRequestBehavior.AllowGet);
         }
@@ -42,39 +52,40 @@ namespace ConversationTracker.Controllers
         [HttpPost]
         public JsonResult saveTrackedConversation(ConversationTrackerObject convo)
         {
-            using (IDocumentStore store = new DocumentStore
-            {
-                Url = "mongodb://appharbor_62031bdd-72a5-4bf1-9b12-4977e6e6b49b:e91nm4msb5f4k87kblf7ha42pi@ds031972.mongolab.com:31972/appharbor_62031bdd-72a5-4bf1-9b12-4977e6e6b49b"
-            })
-            {
-                store.Initialize();
-                using (IDocumentSession session = store.OpenSession())
-                {
-                    session.Store(convo);
-                    session.SaveChanges();
-                }
-            }
+            //using (IDocumentStore store = new DocumentStore
+            //{
+            //    Url = "mongodb://appharbor_62031bdd-72a5-4bf1-9b12-4977e6e6b49b:e91nm4msb5f4k87kblf7ha42pi@ds031972.mongolab.com:31972/appharbor_62031bdd-72a5-4bf1-9b12-4977e6e6b49b"
+            //})
+            //{
+            //    store.Initialize();
+            //    using (IDocumentSession session = store.OpenSession())
+            //    {
+            //        session.Store(convo);
+            //        session.SaveChanges();
+            //    }
+            //}
             return Json(convo);
         }
 
         [HttpPost]
         public JsonResult deleteTrackedConversation(ConversationTrackerObject convo)
         {
-            List<ConversationTrackerObject> fromDb;
-            using (IDocumentStore store = new DocumentStore
-            {
-                Url = "mongodb://appharbor_62031bdd-72a5-4bf1-9b12-4977e6e6b49b:e91nm4msb5f4k87kblf7ha42pi@ds031972.mongolab.com:31972/appharbor_62031bdd-72a5-4bf1-9b12-4977e6e6b49b"
-            })
-            {
-                store.Initialize();
-                using (IDocumentSession session = store.OpenSession())
-                {
-                    session.Advanced.Defer(new DeleteCommandData { Key = convo.Id });
-                    session.SaveChanges();
-                    fromDb = session.Query<ConversationTrackerObject>().ToList();
-                }
-            }
-            return Json(fromDb);
+            //List<ConversationTrackerObject> fromDb;
+            //using (IDocumentStore store = new DocumentStore
+            //{
+            //    Url = "mongodb://appharbor_62031bdd-72a5-4bf1-9b12-4977e6e6b49b:e91nm4msb5f4k87kblf7ha42pi@ds031972.mongolab.com:31972/appharbor_62031bdd-72a5-4bf1-9b12-4977e6e6b49b"
+            //})
+            //{
+            //    store.Initialize();
+            //    using (IDocumentSession session = store.OpenSession())
+            //    {
+            //        session.Advanced.Defer(new DeleteCommandData { Key = convo.Id });
+            //        session.SaveChanges();
+            //        fromDb = session.Query<ConversationTrackerObject>().ToList();
+            //    }
+            //}
+            //return Json(fromDb);
+            return Json(null);
         }
     }
 }

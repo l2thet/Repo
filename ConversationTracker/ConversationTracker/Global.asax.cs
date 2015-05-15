cb42c6e7cd1044deb862759caf6be7d2e7cc781b
l2thet <daltvet@hotmail.com>
2015-05-14 23:59:02 -0400

    Bug fix for redirect loop.
    Bug fix for redirect loop.

--- ConversationTracker/ConversationTracker/Global.asax.cs
+++ ConversationTracker/ConversationTracker/Global.asax.cs
@@ -21,17 +21,5 @@ namespace ConversationTracker
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
-
-        //protected void Application_BeginRequest() {
-        //    if (!Context.Request.Url.ToString().Contains("localhost"))
-        //    {
-        //        if (Context.Request.Url.ToString().Contains("http:"))
-        //        {
-        //            string redirectstring = Context.Request.Url.ToString().Replace("http:", "https:");
-        //            redirectstring = Regex.Replace(redirectstring, ":.*", "", RegexOptions.IgnoreCase);
-        //            Response.Redirect(redirectstring);
-        //        }
-        //    }
-        //}
     }
 }
